c87507c2f1b4d3031a9b15f0f0817c39a2687751
Tyan <tyan.dev.andriansyah@gmail.com>
2024-09-03 11:02:09 +0700

    fix error build

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,7 @@
-import LoginForm from "@/components/auth/login-form";
+// import LoginForm from "@/components/auth/login-form";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
+  return <div>{/* <LoginForm /> */}</div>;
 };
 
 export default LoginPage;
